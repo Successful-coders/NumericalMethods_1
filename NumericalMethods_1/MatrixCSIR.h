dfe186fc1434588c0664ee0b94ffb973307c0994
Wings' might <wingsmight@gmail.com>
2020-02-29 16:18:49 +0700

    merge

--- NumericalMethods_1/MatrixCSIR.h
+++ NumericalMethods_1/MatrixCSIR.h
@@ -1,16 +1,12 @@
 #pragma once
-<<<<<<< HEAD
 #include <vector> 
 
 using namespace std;
-=======
-#include <vector>
->>>>>>> dbd939ce70e765a9efeea2aab60c3cb81b621067
 
 class MatrixCSIR
 {
 public:
-	//MatrixCSIR(int sizeMatrix, std::vector <double> di, std::vector <double> autr, std::vector <double> altr, std::vector <int> jptr, std::vector <int> iptr); рср ньхайю
+	MatrixCSIR(int sizeMatrix, vector <double> di, vector <double> autr, vector <double> altr, vector <int> jptr, vector <int> iptr);
 	double GetElement(int row, int column);
 	void TransposeFullMatrix();
 	void MultiplyFullMatrix();
@@ -18,15 +14,11 @@ public:
 	void FiftenExs();
 
 private:
-<<<<<<< HEAD
 	int size = 0;
-=======
-	int sizeMatrix = 0;
+
 	std::vector <double> di;
 	std::vector <double> autr;
 	std::vector <double> altr;
 	std::vector <int> jptr;
 	std::vector <int> iptr;
 };
->>>>>>> dbd939ce70e765a9efeea2aab60c3cb81b621067
-
