7c4bbd85b14b83b7ad01869140a122bf6df3c574
Voronik <voronkina.2018@azure.nstu.ru>
2020-02-29 15:12:27 +0700

    read

--- NumericalMethods_1/Main.cpp
+++ NumericalMethods_1/Main.cpp
@@ -2,12 +2,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-using namespace std;//лучше потом не использоать
+//using namespace std;//лучше потом не использоать
 
 // надо будет сделать с шаблонами на тип int & double, есть пример в методе
-void ReadBin(char* filename)
+class Sparse_Matrix_CSlR
 {
+public:
+	int sizeMatrix = 0;
+	// Динамику?
+	double *di = new double[] ;
+	double *autr = new double[];
+	double *altr = new double[];
+	int *jptr = new int[];
+	int *iptr = new int[];
+	Sparse_Matrix_CSlR(const std::string& PATH);
+};
 
+//class Reader
+//{
+//public:
+//
+//private:
+//
+//};
+
+Sparse_Matrix_CSlR::Sparse_Matrix_CSlR(const std:: string &PATH)
+{
+	//размер матрицы
+	int N = 0;
+	std::ifstream Reader(PATH + "size.bin", std::ios::binary);
+	if (!Reader.is_open())
+		throw std::exception("File size.bin was not found...");
+	Reader.read((char*)&N, sizeof(N));
+	Reader.close();
+	//di
+	Reader.open(PATH + "di.bin", std::ios::binary);
+	if (!Reader.is_open())
+		throw std::exception("File di.bin was not found...");
+	di.resize(N);
+	for (int i = 0; i < N; i++)
+		Reader.read((char*)&di[i], sizeof(double));
+	Reader.close();
+	//iptr
+	Reader.open(PATH + "iptr.bin", std::ios::binary);
+	if (!Reader.is_open())
+		throw std::exception("File iptr.bin was not found...");
+	iptr.resize(N + 1);
+	for (int i = 0; i < N + 1; i++)
+		Reader.read((char*)&iptr[i], sizeof(int));
+	Reader.close();
+	//jptr
+	Reader.open(PATH + "jptr.bin", std::ios::binary);
+	if (!Reader.is_open())
+		throw std::exception("File jptr.bin was not found...");
+	int JPTR_SIZE = iptr[N] - 1;
+	jptr.resize(JPTR_SIZE);
+	for (int i = 0; i < JPTR_SIZE; i++)
+		Reader.read((char*)&jptr[i], sizeof(int));
+	Reader.close();
+	/* Аналогично заполняются массивы altr и autr */
 }
 
 void TransFullMatrix()
@@ -31,14 +84,8 @@ void FiftenExs()
 }
 int main()
 {
-	char* fileName;
-	int sizeMatrix = 0;
-	// Динамику?
-	double di[sizeMatrix];
-	double autr[sizeMatrix];
-	double altr[sizeMatrix];
-	int jptr[sizeMatrix];
-	int iptr[sizeMatrix];
+	
+
 	ReadBin(fileName);
 
 }
