652934758cbfeac0f9a2faa5bd6721ae7586bb54
Wings' might <wingsmight@gmail.com>
2020-02-29 12:31:29 +0700

    create starting structure

---
+++ NumericalMethods_1/Main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;//лучше потом не использоать
+
+// надо будет сделать с шаблонами на тип int & double, есть пример в методе
+void ReadBin(char* filename)
+{
+
+}
+
+void TransFullMatrix()
+{
+	// перевод в полный формат по заданию
+}
+
+void MultipFullMatrix()
+{
+	// умножение плотной матрицы на произвольный вектор
+}
+
+void MultipRazrMatrix()
+{
+	//умножение разреженой матрицы на вектор
+}
+
+void FiftenExs()
+{
+	// пятое задание, хз что там
+}
+int main()
+{
+	char* fileName;
+	int sizeMatrix = 0;
+	// Динамику?
+	double di[sizeMatrix];
+	double autr[sizeMatrix];
+	double altr[sizeMatrix];
+	int jptr[sizeMatrix];
+	int iptr[sizeMatrix];
+	ReadBin(fileName);
+
+}
